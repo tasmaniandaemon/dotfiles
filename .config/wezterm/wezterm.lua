78bf0bfd51e55e8605f544ef35a0ca954eb7a3e2
tasmaniandaemon <3520266+tasmaniandaemon@users.noreply.github.com>
2024-10-04 01:45:51 +0300

    Wezterm colorscheme update to gruvbox

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -95,7 +95,7 @@ local config = {
 	-- https://github.com/wez/wezterm/issues/1772
 	-- https://github.com/wez/wezterm/issues/5103
 	-- enable_wayland = false,
-	color_scheme = "nordfox",
+	color_scheme = 'Gruvbox dark, hard (base16)',
 	color_scheme_dirs = { os.getenv("HOME") .. "/.config/wezterm/colors/" },
 	hide_tab_bar_if_only_one_tab = false,
 	adjust_window_size_when_changing_font_size = false,
